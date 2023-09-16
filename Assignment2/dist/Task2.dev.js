364b9eb96092c918d4dc8c1cacd7f7f963fdcf55
islamatiyat01 <islamatiyat50@yahoo.com>
2023-09-16 11:48:56 +0300

    task 2 upload solution

--- Assignment2/dist/Task2.dev.js
+++ Assignment2/dist/Task2.dev.js
@@ -1,7 +1,10 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 // 2. Write a function called contains that searches
 // for a value in a nested object.It returns true if the object contains that value.
 // Sample Object:
-
 // var nestedObject = {
 //   data: {
 //     info: {
@@ -9,7 +12,6 @@
 //         thing: {
 //           moreStuff: {
 //             magicNumber: 44,
-
 //             something: "foo2",
 //           },
 //         },
@@ -17,25 +19,25 @@
 //     },
 //   },
 // };
-
 // Solution:
-
 function contains(obj, targetValue) {
-  for (const key in obj) {
+  for (var key in obj) {
     if (obj[key] === targetValue) {
       return true;
-    }
-    // If the property's value is an object, recursively search inside it
-    if (typeof obj[key] === "object" && !Array.isArray(obj[key])) {
+    } // If the property's value is an object, recursively search inside it
+
+
+    if (_typeof(obj[key]) === "object" && !Array.isArray(obj[key])) {
       if (contains(obj[key], targetValue)) {
         return true;
       }
     }
   }
-  return false;
-}
 
-// Sample Object
+  return false;
+} // Sample Object
+
+
 var nestedObject = {
   data: {
     info: {
@@ -43,17 +45,15 @@ var nestedObject = {
         thing: {
           moreStuff: {
             magicNumber: 44,
-            something: "foo2",
-          },
-        },
-      },
-    },
-  },
-};
+            something: "foo2"
+          }
+        }
+      }
+    }
+  }
+}; // Value to search for
 
-// Value to search for
 var targetValue = 44;
-
 var isContained = contains(nestedObject, targetValue);
-
 console.log(isContained); // true,
+//# sourceMappingURL=Task2.dev.js.map
